6f3654374c60e2a18e9a2f1422d84e3f7a8809e8
EnzoS01 <cocenzosilva@gmail.com>
2023-11-11 01:35:16 -0300

    Update ProductServices.ts

--- src/services/ProductServices.ts
+++ src/services/ProductServices.ts
@@ -1,53 +1,63 @@
 import { Product } from "../types/Product";
 
-const BASE_URL='https://fakestoreapi.com'
+const BASE_URL = 'https://fakestoreapi.com';
 
-export const ProductServices={
+export const ProductServices = {
+
     
-    getProducts: async(): Promise<Product[]>=> {
-        const response= await fetch(`${BASE_URL}/products`);
-        const data= await response.json();
-        return data;
-    },   
-
-    getProduct:async(id: number): Promise<Product>=>{;
-        const response= await fetch(`${BASE_URL}/products/${id}`)
-        const data= await response.json();
+    getProducts: async (): Promise<Product[]> => {
+       
+        const response = await fetch(`${BASE_URL}/products`);
+        const data = await response.json();
         return data;
     },
 
-    createProduct: async(product: Product): Promise<Product>=>{
-        const response= await fetch(`${BASE_URL}/products`,{
-        method: "POST",
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body:JSON.stringify(product)
-    });
-        const data= await response.json();
+    
+    getProduct: async (id:number): Promise<Product> => {
+
+        const response = await fetch (`${BASE_URL}/products/${id}`);
+        const data = await response.json();
         return data;
+        
     },
 
-    updateProduct: async (id:number, product:Product ):Promise <Product> =>{
-        const response =await fetch(`${BASE_URL}/products`,{
-            method: "PUT",
-            headers:{
+    createProduct:async (product:Product):Promise<Product> => {
+
+        const response = await fetch(`${BASE_URL}/products`, {
+            method: "POST",
+            headers: {
                 'Content-Type': 'application/json'
             },
-            body:JSON.stringify(product)
+            body: JSON.stringify(product)
         });
-        const data= await response.json();
-        return data;
 
+        const data = await response.json();
+        return data;
+        
     },
+
+    updateProduct: async (id: number, product: Product): Promise<Product> => {
+        
+        const response = await fetch(`${BASE_URL}/products/${id}`, {
+            method: "PUT",
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(product)
+        });
+
+        const data = await response.json();
+        return data;
+    },
+
     
-    deleteProduct: async(id:number): Promise<void>=>
-    {
-        await fetch(`${BASE_URL}/products/${id}`,{
-            method: "DELETE",
+
+    deleteProduct: async (id:number): Promise<void> => {
+        await fetch(`${BASE_URL}/products/${id}`, {
+            method: "DELETE"
         });
     }
+    
 
-
-
+  
 }
